671111cb994432f9ae9c27c8f5833337c6016082
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-23 23:56:48 +0900

    Update listening_quiz_2.py

--- pages/listening_quiz_2.py
+++ pages/listening_quiz_2.py
@@ -78,7 +78,7 @@ def generate_dialogue_audio(dialogue):
     
     for speaker, lines in speakers.items():
         text = " ".join(lines)
-        voice = "onyx" if speaker == "A" else "onix"  # A는 여성 목소리, B는 남성 목소리
+        voice = "onix" if speaker == "A" else "onix"  # A는 여성 목소리, B는 남성 목소리
         audio_tag = text_to_speech(text, voice)
         audio_tags.append(audio_tag)
     
